5751f58b7f3335d8768ee76de0ce9d6b4122fe0a
dualsine <dualsine@gmail.com>
2018-01-16 14:24:04 +0100

    init

---
+++ client/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <div className="logo">App</div>
+  </div>
+);
+
+export default Header;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3000 : process.env.PORT;
+
+const app = express();
+
+app.use(express.static(__dirname + '../dist'));
+
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackConfig = require('../webpack.config.js');
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
+app.listen(port, '0.0.0.0', function(err) {
+  if (err)
+    console.log(err);
+  console.info('Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
